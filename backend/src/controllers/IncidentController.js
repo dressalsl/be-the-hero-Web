fb9e62f56d9811e698b4ef41abc8fc5a2bca2834
Andressa <dressalsl97@gmail.com>
2020-03-26 18:13:43 -0300

    backend ok

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,24 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(request, response){
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents')
+            .count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id' ,"=","incidents.ong_id")
+            .limit(5) // limitar 5 registros por pg
+            .offset((page-1) * 5)
+            .select([
+                'incidents.*', 
+                'ongs.name',
+                'ongs.email', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 // antes de todas as reqs, antes das rotas converte o json da req em obj
+app.use(cors());
 app.use(express.json()); 
 app.use(routes);
 
